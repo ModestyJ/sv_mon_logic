4131f102362dc2aa889c2f638835b385f8c7b8cd
ModestyJ <modesty.j@gmail.com>
2022-06-21 22:03:18 +0900

    Update README and ModelSim related issues

---
+++ sv_mon_pkg.sv
@@ -0,0 +1,4 @@
+package sv_mon_pkg;
+    `include "sv_mon_logic/sram_mon.sv"
+    `include "sv_mon_logic/utilization_mon.sv"
+endpackage
--- sv_mon_pkg.svh
+++
@@ -1,4 +0,0 @@
-package sv_mon_pkg;
-    `include "sram_mon.sv"
-    `include "utilization_mon.sv"
-endpackage

620a78d159aea37b33d023cb052b04b399e5aa96
lukpcm <lukpcm@naver.com>
2022-12-12 19:21:36 +0900

    Change filepath

--- sv_mon_pkg.sv
+++ sv_mon_pkg.sv
@@ -1,4 +1,4 @@
 package sv_mon_pkg;
-    `include "sv_mon_logic/sram_mon.sv"
-    `include "sv_mon_logic/utilization_mon.sv"
+    `include "../tb/models/sv_mon_logic/sram_mon.sv"
+    `include "../tb/models/sv_mon_logic/utilization_mon.sv"
 endpackage
